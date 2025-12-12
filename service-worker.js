1ca98810b72a4679650c76b7f5239fbf91af1123
Vitor Rodrigues Gomes <vitor_jackson1992@hotmail.com>
2025-12-12 11:47:46 -0300

    ajuste workerjs/manifestJson

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,28 @@
-const CACHE_NAME = "consulta-lojas-v1";
+const CACHE_NAME = 'consulta-lojas-v1';
 
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/cadastro.html",
-  "/manifest.json",
+  '/',
+  '/index.html',
+  '/cadastro.html',
+  '/manifest.json',
 
-  "/assets/css/style.css",
+  // CSS
+  '/assets/css/style.css',
 
-  "/assets/js/db.js",
-  "/assets/js/busca.js",
-  "/assets/js/cadastro.js",
-  "/assets/js/firebase.js",
-  "/assets/js/sync.js"
+  // JS
+  '/assets/js/busca.js',
+  '/assets/js/cadastro.js',
+  '/assets/js/db.js',
+  '/assets/js/firebase.js',
+  '/assets/js/sync.js',
+
+  // Ícones
+  '/assets/image/icon-192.png',
+  '/assets/image/icon-512.png'
 ];
 
-// Instala o cache
-self.addEventListener("install", event => {
+// Instalação
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
@@ -25,8 +31,8 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-// Ativa o service worker
-self.addEventListener("activate", event => {
+// Ativação (limpa caches antigos)
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
@@ -41,8 +47,8 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-// Busca offline
-self.addEventListener("fetch", event => {
+// Fetch
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
